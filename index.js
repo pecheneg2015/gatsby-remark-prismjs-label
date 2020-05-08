88708db3559366f6304805c2e24b16afafaed29d
d.pechenin <pecheneg@student.su>
2020-05-09 02:12:36 +0300

    Added test data

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
 "use strict";
 
 // const visit = require("unist-util-visit");
+let separator = ":";
+let test={
+    type: 'root',
+    children: [
+      {
+        type: 'heading',  
+        depth: 2,
+        children: "",
+        position: "",
+      },
+      { type: 'paragraph', children: "", position: "" },
+      {
+        type: 'list',
+        ordered: false,
+        start: null,
+        spread: false,
+        children: "",
+        position: ""
+      },
+      {
+        type: 'code',
+        lang: 'json:label:test',
+        meta: null,
+        value: '{\n  "compilerOptions": {\n    ···\n    "allowJs": true\n  }\n}',
+        position: ""
+      },
+      {
+        type: 'code',
+        lang: 'javascript',
+        meta: null,
+        value: '/**\n' +
+          ' * @param {number} x - A number param.\n' +
+          ' * @param {number} y - A number param.\n' +
+          ' * @returns {number} This is the result\n' +
+          ' */\n' +
+          'function add(x, y) {\n' +
+          '  return x + y;\n' +
+          '}\n' +
+          '/** @typedef {{ prop1: string, prop2: string, prop3?: number }} SpecialType */\n' +
+          '/** @typedef {(data: string, index?: number) => boolean} Predicate */',
+        position: ""
+      },
+    ],
+    position: {
+      start: { line: 1, column: 1, offset: 0 },
+      end: { line: 71, column: 236, offset: 4787 }
+    }
+  }
+
+  let data = {};
+  data.markdownAST = test;
 
 const titlePrefix = ":title="
 
-module.exports = function gatsbyRemarkCodeTitles(_ref, options) {
+
+
+// module.exports = 
+function gatsbyRemarkCodeTitles(_ref, options) {
   const markdownAST = _ref.markdownAST;
   console.log(markdownAST)
   const customClassName = options.className;
@@ -33,3 +87,5 @@ module.exports = function gatsbyRemarkCodeTitles(_ref, options) {
 //   });
   return markdownAST;
 };
+
+gatsbyRemarkCodeTitles(data,{})
